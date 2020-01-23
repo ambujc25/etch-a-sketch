bd9c95ad0e73498c95af89c9168bf2adda778b5c
Ambuj <ambuj2122@gmail.com>
2020-01-23 18:42:44 +0530

    Added all functionality

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
 const container = document.getElementById("container");
 
 function makeRows(rows,cols){
-    
+    container.style.setProperty('--rows',rows);
+    container.style.setProperty('--columns',cols);    
+
+    for(i=0; i<rows*cols; i++){
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+    };
+
+};
+
+makeRows(16,16);
+
+function color_grid(){
+    const cells = document.querySelectorAll(".grid-item");
+    cells.forEach((c) => {
+        c.addEventListener('mouseover', (e)=> {
+            c.style.cssText = "background-color: black";
+        });
+    });
 }
+
+color_grid();
+
+const button = document.querySelector('button');
+
+button.addEventListener('click',(e) => {
+    const cells = document.querySelectorAll(".grid-item");
+    cells.forEach((c) => {
+        c.style.cssText = "background-color:white";
+    });
+
+    let row = prompt("Enter number of rows: ");
+    let column = prompt("Enter number of columns: ");
+
+    let child = container.lastElementChild;
+    while(child){
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+    
+    makeRows(row,column);
+    color_grid();
+});
